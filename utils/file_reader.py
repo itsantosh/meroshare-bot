eebcfea968abd828dd710a5f4b43e3fe2d17dee2
santosh silwal <itsantoshsilwal@gmail.com>
2023-07-24 14:26:25 +0545

    false dmat user counts
    when extra line added in dmats.txt it was counted as a dmat users so remove empty line.

--- utils/file_reader.py
+++ utils/file_reader.py
@@ -5,5 +5,6 @@ def file_reader(file_name):
 
     with open(file_name, 'r') as f:
         # create a list of each line of file after splitting it by comma
-        return [line.split(',') for line in f.readlines()]
+        return [line.split(',') for line in f.readlines() if line.strip()] 
+
 
